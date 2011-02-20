df80300828a069f3aba570b19ba3726ea699fd60
brad.froehle <brad.froehle@76247baa-f0f0-11de-a8d5-974f63da035b>
2011-02-20 21:15:10 +0000

    Tagging version 1.06

---
+++ tags/1.06/BART.wdgt/Maps/Euro/info.js
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+//  Euro/info.js is part of the BART dashboard widget.  (c) 2005 Bret Victor
+//  This software is licensed under the terms of the open source MIT license.
+//------------------------------------------------------------------------------
+//
+//  The "Euro" map was designed by Omid Tavallai.
+//
+
+map = new Map("Euro")
+
+map.width  = 746  // px
+map.height = 670  // px
+
+map.isZoomable = true
+ 
+map.addStation("Richmond",270,72)
+map.addStation("Del Norte",324,95)
+map.addStation("Plaza",338,116)
+map.addStation("North Berkeley",363,149)
+map.addStation("Berkeley",363,172)
+map.addStation("Ashby",364,194)
+map.addStation("Rockridge",399,185)
+map.addStation("Orinda",429,159)
+map.addStation("Lafayette",468,146)
+map.addStation("Walnut Creek",508,138)
+map.addStation("Pleasant Hill",538,109)
+map.addStation("Concord",571,65)
+map.addStation("North Concord",570,28)
+map.addStation("Pittsburg",637,27)
+map.addStation("MacArthur",370,222)
+map.addStation("19th St",370,242)
+map.addStation("12th St",370,264)
+map.addStation("West Oakland",330,301)
+map.addStation("Embarcadero",228,302)
+map.addStation("Montgomery",210,317)
+map.addStation("Powell",198,333)
+map.addStation("Civic Center",183,351)
+map.addStation("16th St",167,371)
+map.addStation("24th St",156,388)
+map.addStation("Glen Park",139,413)
+map.addStation("Balboa Park",123,437)
+map.addStation("Daly City",122,462)
+map.addStation("Colma",112,509)
+map.addStation("South SF",167,523)
+map.addStation("San Bruno",194,531)
+map.addStation("SFO",255,540)
+map.addStation("Millbrae",265,591)
+map.addStation("Lake Merritt",381,330)
+map.addStation("Fruitvale",402,352)
+map.addStation("Coliseum",424,372)
+map.addStation("San Leandro",458,406)
+map.addStation("Bay Fair",482,430)
+map.addStation("Castro Valley",540,446)
+map.addStation("West Dublin",617,446)
+map.addStation("Dublin",690,446)
+map.addStation("Hayward",511,481)
+map.addStation("South Hayward",531,539)
+map.addStation("Union City",583,592)
+map.addStation("Fremont",634,642)
